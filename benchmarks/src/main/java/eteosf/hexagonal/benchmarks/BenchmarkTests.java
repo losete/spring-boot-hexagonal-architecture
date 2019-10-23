75a524c371874da505a05ed3b7205cfa929ca97f
losete <losetazo@gmail.com>
2019-10-23 10:57:43 +0200

    Merge with master + solved style problems

--- benchmarks/src/main/java/eteosf/hexagonal/benchmarks/BenchmarkTests.java
+++ benchmarks/src/main/java/eteosf/hexagonal/benchmarks/BenchmarkTests.java
@@ -1,20 +1,27 @@
 package eteosf.hexagonal.benchmarks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import eteosf.hexagonal.domain.model.Product;
 import java.util.concurrent.TimeUnit;
-import org.openjdk.jmh.annotations.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.TimeValue;
 
-public class BenchmarkTests{
+public class BenchmarkTests {
 
+  /**
+   * Benchmark for testGetProductID.
+   */
   @Benchmark
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  public void benchmarkTestGetProductID(){
+  public void benchmarkTestGetProductID() {
     int productId = 1;
     String name = "Beer";
     String description = "7%";
@@ -22,6 +29,10 @@ public class BenchmarkTests{
     assertEquals((Integer) productId, p.getProductId());
   }
 
+  /**
+   * Main method.
+   * @param args args if required
+   */
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
         .include(BenchmarkTests.class.getSimpleName())
