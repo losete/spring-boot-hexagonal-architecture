41b1d4c8181f96eec5213062babb7b8acd9c3409
losete <losetazo@gmail.com>
2019-10-23 12:08:30 +0200

    Spotbugs failOnError set to false

--- benchmarks/src/main/java/eteosf/hexagonal/benchmarks/BenchmarkTests.java
+++ benchmarks/src/main/java/eteosf/hexagonal/benchmarks/BenchmarkTests.java
@@ -30,6 +30,77 @@ public class BenchmarkTests {
   }
 
   /**
+   * Benchmark for testSetProductID.
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkTestSetProductID() {
+    int productId = 1;
+    String name = "Beer";
+    String description = "7%";
+    Product p = new Product(productId, name, description);
+    productId = 2;
+    p.setProductId(productId);
+    assertEquals((Integer) productId, p.getProductId());
+  }
+
+  /**
+   * Benchmark for testGetName.
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkTestGetName() {
+    int productId = 1;
+    String name = "Beer";
+    String description = "7%";
+    Product p = new Product(productId, name, description);
+    assertEquals(name, p.getName());
+  }
+
+  /**
+   * Benchmark for testSetName.
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkTestSetName() {
+    int productId = 1;
+    String name = "Beer";
+    String description = "7%";
+    Product p = new Product(productId, name, description);
+    name = "Beeer";
+    p.setName(name);
+    assertEquals(name, p.getName());
+  }
+
+  /**
+   * Benchmark for testGetDescription.
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkTestGetDescription() {
+    int productId = 1;
+    String name = "Beer";
+    String description = "7%";
+    Product p = new Product(productId, name, description);
+    assertEquals(description, p.getDescription());
+  }
+
+  /**
+   * Benchmark for testSetDescription.
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void benchmarkTestSetDescription() {
+    int productId = 1;
+    String name = "Beer";
+    String description = "7%";
+    Product p = new Product(productId, name, description);
+    description = "13%";
+    p.setDescription(description);
+    assertEquals(description, p.getDescription());
+  }
+
+  /**
    * Main method.
    * @param args args if required
    */
