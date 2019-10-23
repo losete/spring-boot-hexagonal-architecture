83af2330a8f9a47d03ee9cfae5fa32d4b4b74f0e
losete <losetazo@gmail.com>
2019-10-23 10:42:01 +0200

    Added dependency + Added main to BenchmarkTests

--- benchmarks/src/main/java/eteosf/hexagonal/benchmarks/BenchmarkTests.java
+++ benchmarks/src/main/java/eteosf/hexagonal/benchmarks/BenchmarkTests.java
@@ -4,19 +4,34 @@ import eteosf.hexagonal.domain.model.Product;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
-public class BenchmarkTests {
+public class BenchmarkTests{
 
   @Benchmark
   @OutputTimeUnit(TimeUnit.MILLISECONDS)
-  @Warmup(iterations = 5)
-  @Measurement(iterations = 10)
-  @BenchmarkMode(Mode.AverageTime)
   public void benchmarkTestGetProductID(){
     int productId = 1;
     String name = "Beer";
     String description = "7%";
     Product p = new Product(productId, name, description);
     assertEquals((Integer) productId, p.getProductId());
+  }
+
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+        .include(BenchmarkTests.class.getSimpleName())
+        .warmupTime(TimeValue.milliseconds(1))
+        .warmupIterations(5)
+        .measurementTime(TimeValue.milliseconds(1))
+        .measurementIterations(5)
+        .mode(Mode.AverageTime)
+        .build();
+
+    new Runner(opt).run();
   }
 }
